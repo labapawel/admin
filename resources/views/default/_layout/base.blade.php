056fc6593d09465517f932626c58d8c31218870e
Paweł Łaba <labapawel@gmail.com>
2025-03-31 19:40:07 +0200

    zmiany

--- resources/views/default/_layout/base.blade.php
+++ resources/views/default/_layout/base.blade.php
@@ -11,7 +11,7 @@
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
-
+	@stack('style')
 	@stack('scripts')
 </head>
 <body class="{{ config('sleeping_owl.body_default_class', 'sidebar-mini sidebar-open') . (@$_COOKIE['sidebar-state'] == 'sidebar-collapse' ? ' sidebar-collapse' : '') . (@$_COOKIE['theme-mode'] == 'dark' ? ' dark-mode' : '') }}">
